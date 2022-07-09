5d5766977399bd02636909fb958394e5de031b1e
ismailabdurrahimnf <92084656+ISMAILABDURRAHIMNF@users.noreply.github.com>
2022-07-09 19:41:35 +0700

    Delete application directory

--- application/views/layout/library_css.php
+++
@@ -1,8 +0,0 @@
-<!-- Google Font: Source Sans Pro -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="<?php echo base_url('plugins/fontawesome-free/css/all.min.css')?>">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?php echo base_url('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')?>">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url('dist/css/adminlte.min.css')?>">
